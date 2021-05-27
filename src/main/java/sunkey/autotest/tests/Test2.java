ae56a7af3ea96e3830b95d88187ea85be24dd0af
zhaoml <7335678@qq.com>
2021-05-27 17:39:06 +0800

    完成登录

--- src/main/java/sunkey/autotest/tests/Test2.java
+++ src/main/java/sunkey/autotest/tests/Test2.java
@@ -2,8 +2,13 @@ package sunkey.autotest.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import sunkey.autotest.Runner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Sunkey
@@ -11,11 +16,43 @@ import sunkey.autotest.Runner;
  **/
 public class Test2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Runner.loadConfig("config.properties");
         FirefoxDriver driver = Runner.FIREFOX.get("http://pre-sso.27aichi.cn");
-        WebElement element = driver.findElement(By.tagName("form"));
-        System.out.println(element);
+        WebElement form = driver.findElement(By.tagName("form"));
+        List<WebElement> inputs = form.findElements(By.tagName("input"));
+
+        Map<String, String> keys = new HashMap<>();
+        keys.put("username", "15776319821");
+        keys.put("password", "123321");
+        keys.put("captcha", "lsls");
+
+        for (WebElement input : inputs) {
+            String key = input.getAttribute("name");
+            String putVal = keys.get(key);
+            if (putVal != null) {
+                input.sendKeys(putVal);
+            }
+        }
+
+        WebElement submit = form.findElement(By.className("pwd-btn"));
+
+        String url = driver.getCurrentUrl();
+        submit.click();
+
+        while (true) {
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("current url:" + currentUrl);
+            if (!url.equals(currentUrl)) {
+                // 页面发生跳转
+                break;
+            }
+            Thread.sleep(100);
+        }
+
+        System.out.println("登陆成功");
+        System.out.println(driver.getTitle());
+
     }
 
 }
