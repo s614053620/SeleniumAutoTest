11f481daaf4364b887463155ea4e5de6183560e0
sunkey <614053620@qq.com>
2021-05-27 17:20:30 +0800

    login

--- src/main/java/sunkey/autotest/tests/Test.java
+++ src/main/java/sunkey/autotest/tests/Test.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import sunkey.autotest.Runner;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Sunkey
@@ -13,14 +15,45 @@ import java.util.List;
  **/
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Runner.loadConfig("config-sunkey.properties");
         ChromeDriver driver = Runner.CHROME.get("http://pre-sso.27aichi.cn");
         WebElement form = driver.findElement(By.tagName("form"));
         List<WebElement> inputs = form.findElements(By.tagName("input"));
+
+        Map<String, String> keys = new HashMap<>();
+        keys.put("username", "15776319821");
+        keys.put("password", "123321");
+        keys.put("captcha", "lsls");
+
         for (WebElement input : inputs) {
-            System.out.println(input.getAttribute("name"));
+            String key = input.getAttribute("name");
+            String putVal = keys.get(key);
+            if (putVal != null) {
+                input.sendKeys(putVal);
+            }
         }
+
+        WebElement submit = form.findElement(By.className("pwd-btn"));
+
+        String url = driver.getCurrentUrl();
+        submit.click();
+
+        while (true) {
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("current url:" + currentUrl);
+            if (!url.equals(currentUrl)) {
+                // 页面发生跳转
+                break;
+            }
+            Thread.sleep(100);
+        }
+
+        System.out.println("登陆成功");
+        System.out.println(driver.getTitle());
+
     }
+
+
 
 }
