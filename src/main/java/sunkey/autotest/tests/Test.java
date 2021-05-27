1afc46261ac639ab7598e0092c48ce1caa9d232a
sunkey <614053620@qq.com>
2021-05-27 16:38:40 +0800

    init

--- src/main/java/sunkey/autotest/tests/Test.java
+++ src/main/java/sunkey/autotest/tests/Test.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import sunkey.autotest.Runner;
 
+import java.util.List;
+
 /**
  * @author Sunkey
  * @since 2021-05-27 3:39 下午
@@ -14,8 +16,11 @@ public class Test {
     public static void main(String[] args) {
         Runner.loadConfig("config-sunkey.properties");
         ChromeDriver driver = Runner.CHROME.get("http://pre-sso.27aichi.cn");
-        WebElement element = driver.findElement(By.tagName("form"));
-        System.out.println(element);
+        WebElement form = driver.findElement(By.tagName("form"));
+        List<WebElement> inputs = form.findElements(By.tagName("input"));
+        for (WebElement input : inputs) {
+            System.out.println(input.getAttribute("name"));
+        }
     }
 
 }
